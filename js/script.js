e40e4bec057ce29287c0f76072ab69671c6dd42f
MicheleBenaco <benaco.michele@yahoo.it>
2022-10-11 16:17:35 +0200

    primoes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let emailUser = document.getElementById('userEmail').value;
+const allEmail = ["mario@gmail.com", "luca@gmail.com", "lucia@gmail.com","marco@gmail.it"]
+
+for(let i = 0; i < allEmail.length; i++){
+    if(emailUser == allEmail){
+        let vero = document.createElement("true_email");
+        vero.innerText = (" Puoi paerteciapre ")
+    }else if( emailUser != allEmail){
+        let falso = document.createElement("falsa_email");
+        falso.innerText = ("Inserisci un email valida per parteciapre");
+    }
+}
+let btn = document.querySelector('button');
