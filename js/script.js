8d331b44f22215fe1e1b1a3818b1422fa7269c22
MicheleBenaco <benaco.michele@yahoo.it>
2022-10-11 17:51:56 +0200

    append

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 // let emailUser = document.getElementById('userEmail').value;
 // const allEmail = ["mario@gmail.com", "luca@gmail.com", "lucia@gmail.com","marco@gmail.it"]
 const bottone = document.getElementById('bottone');
+let emailUser = document.getElementById('userEmail').value;
 bottone.addEventListener('click',
 function () {
     // const bottone = document.getElementById('bottone');
-    let emailUser = document.getElementById('userEmail').value;
+    // let emailUser = document.getElementById('userEmail').value;
     const allEmail = ["mario@gmail.com", "luca@gmail.com", "lucia@gmail.com","marco@gmail.it"]
 
 for(let i = 0; i < allEmail.length; i++){
     if(emailUser == allEmail){
         let vero = document.createElement("true_email");
+        append( "true_email");
         vero.innerText = (" Puoi paerteciapre ")
     }else if( emailUser != allEmail){
         let falso = document.createElement("falsa_email");
+        append( "falsa_email");
         falso.innerText = ("Inserisci un email valida per parteciapre");
     }
 }
 }
 )
 // DADI-------------------------------------------------------------------
+  
